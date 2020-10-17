79d92e2a03c4fab6b7ccd33ac6d9532a3f52d919
Boitumelo Rampete <u17120323@tuks.co.za>
2020-10-17 03:11:53 +0200

    Prelim WC external

--- src/app/pages/external-wildcard-payment/external-wildcard-payment.component.ts
+++ src/app/pages/external-wildcard-payment/external-wildcard-payment.component.ts
@@ -1,15 +1,243 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AfterViewInit, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { StripeService, Elements, Element as StripeElement, ElementsOptions } from 'ngx-stripe';
+import { LoginModalComponent } from 'src/app/modals/login-modal/login-modal.component';
+import { Booking, BookingService } from 'src/app/services/booking/booking.service';
+import { GlobalService } from 'src/app/services/global/global.service';
 
 @Component({
   selector: 'app-external-wildcard-payment',
   templateUrl: './external-wildcard-payment.component.html',
   styleUrls: ['./external-wildcard-payment.component.scss']
 })
-export class ExternalWildcardPaymentComponent implements OnInit {
+export class ExternalWildcardPaymentComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  amount = 0;
+  handler: any;
+  observe$: any;
+
+  verified = false;
+  bookingID: number;
+  loader: boolean;
+  elements: Elements;
+  card: StripeElement;
+
+  loginRef: BsModalRef;
+  bookingInfo: Booking;
+  stripeTest: FormGroup;
+  // optional parameters
+  elementsOptions: ElementsOptions = {
+    locale: 'en'
+  };
+
+  @HostListener('window:popstate' , ['$event'])
+  onpopstate(event) {
+    this.handler.close();
+  }
+  constructor(private fb: FormBuilder, private stripeService: StripeService,
+              private snack: MatSnackBar, private router: Router,
+              private serv: BookingService, private global: GlobalService,
+              private route: ActivatedRoute, private modalService: BsModalService) { }
 
   ngOnInit(): void {
+
+    this.loader = false;
+    this.stripeTest = this.fb.group({
+      name: ['' , Validators.required]
+    });
+
+
+    this.stripeService.elements(this.elementsOptions)
+    .subscribe(elements => {
+      this.elements = elements;
+      // Only mount the element the first time
+      if (!this.card) {
+        this.card = this.elements.create('card', {
+          iconStyle: 'solid',
+          style: {
+            base: {
+              iconColor: '#c4f0ff',
+              color: '#fff',
+              fontWeight: 500,
+              fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+              fontSize: '16px',
+              fontSmoothing: 'antialiased',
+              ':-webkit-autofill': {
+                color: '#fce883',
+              },
+              '::placeholder': {
+                color: '#87BBFD',
+              },
+            },
+            invalid: {
+              iconColor: '#FFC7EE',
+              color: '#FFC7EE',
+            },
+          },
+        });
+        this.card.mount('#example1-card');
+        // registerElements([this.card], 'example1');
+    }
+  });
   }
 
+  ngAfterViewInit() {
+    const sess =  JSON.parse(sessionStorage.getItem('session'));
+
+    if (sess) {
+      // Do something
+    } else {
+      this.observe$ = this.route.params.subscribe(parameter => {
+        // tslint:disable-next-line: no-string-literal
+        this.bookingID = Number(parameter['id']);
+        console.log(parameter['id']);
+        this.LoginModal();
+      });
+    }
+  }
+
+  LoginModal() {
+    this.loginRef = this.modalService.show(LoginModalComponent,
+      {
+        class: 'modal-md modal-dialog-centered',
+        backdrop: 'static',
+        initialState: {
+          data: {
+          message: 'Are you sure you want to remove this guest?'
+          }
+        }
+      });
+    this.loginRef.content.closeBtnName = 'Close';
+    this.loginRef.content.event.subscribe(res => {
+      if (res.result === true) {
+        const session = JSON.parse(sessionStorage.getItem('session'));
+
+        const bookingVerify = {
+          BookingID: this.bookingID,
+          Session: session
+        };
+
+        this.serv.VerifyPayment(bookingVerify, this.global.GetServer()).subscribe(result => {
+          if (result.Session) {
+            sessionStorage.setItem('session', JSON.stringify(result.Session));
+            if (result.Late) {
+              this.snack.open('Your payment period has expired and your booking has been cancelled', 'OK', {
+                horizontalPosition: 'center',
+                verticalPosition: 'top'
+              });
+
+              this.router.navigateByUrl('');
+            } else {
+              if (result.Verified) {
+                this.verified = true;
+                this.amount = result.Amount;
+              } else {
+                if (res.Found) {
+                  this.snack.open('This booking does not seem to belong you.', 'OK', {
+                    horizontalPosition: 'center',
+                    verticalPosition: 'top'
+                  });
+                  this.router.navigateByUrl('');
+                } else {
+                  this.snack.open('This booking has been paid for already.', 'OK', {
+                    horizontalPosition: 'center',
+                    verticalPosition: 'top'
+                  });
+                  sessionStorage.removeItem('session');
+                  this.router.navigateByUrl('');
+                }
+              }
+            }
+          } else {
+            sessionStorage.removeIteme('session');
+            this.router.navigateByUrl('');
+          }
+        });
+      } else {
+        this.router.navigateByUrl('');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.observe$.unsubscribe();
+  }
+
+  openSnack(message) {
+    this.snack.open(message, 'Okay', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  }
+
+  handlePayment() {
+
+    const bookingPay: any = {};
+    this.serv.getClientFromSession(JSON.parse(sessionStorage.getItem('session')), this.global.GetServer()).subscribe(res => {
+      console.log(res);
+      bookingPay.ClientID = res.ClientID;
+      localStorage.setItem('itinerary', JSON.stringify(this.bookingInfo));
+      console.log(this.bookingInfo);
+      sessionStorage.setItem('session', JSON.stringify(res.Session));
+    }, (error: HttpErrorResponse) => this.openSnack(error.message));
+    const name = this.stripeTest.get('name').value;
+    this.stripeService
+      .createToken(this.card, {})
+      .subscribe(result => {
+        if (result) {
+          // Use the token to create a charge or a customer
+          // https://stripe.com/docs/charges
+          this.loader = true;
+          bookingPay.paymentToken = result.token.id;
+          bookingPay.PaymentAmount = this.amount;
+          bookingPay.BookingID = this.bookingID;
+          // this.bookingInfo.paymentToken = result.token.id;
+          // this.bookingInfo.PaymentAmount = this.amount;
+          // this.bookingInfo.BookingID = this.bookingID;
+          const sess = JSON.parse(sessionStorage.getItem('session'));
+          bookingPay.Session = sess;
+          this.serv.PayOutstanding(bookingPay , this.global.GetServer()).subscribe(data => {
+            if (data.Success) {
+              this.loader = false;
+              sessionStorage.setItem('session', JSON.stringify(data.Session));
+              this.snack.open('You have successfully paid for your booking', 'OK', {
+                horizontalPosition: 'center',
+                verticalPosition: 'bottom',
+                duration: 5000
+              });
+              this.router.navigateByUrl('bookingSuccess');
+            } else {
+              if (data.Session.Error) {
+                const login = this.snack.open(data.Session.Error, 'Okay', {
+                  horizontalPosition: 'center',
+                  verticalPosition: 'bottom'
+                });
+
+                login.afterDismissed().subscribe(() => this.router.navigateByUrl('Login'));
+              } else {
+                this.openSnack(data.Message);
+              }
+            }
+          });
+        } else if (result.error) {
+          // Error creating the token
+          this.snack.open(result.error.message, 'OK', {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'bottom'
+          });
+        }
+      });
+  }
+
+  Success() {
+    this.snack.open('Unsuccessful Payment', 'Okay', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+  }
 }
